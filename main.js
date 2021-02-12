3479931b183658faab2e1c6959dd916f9e8a4bee
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-02-12 17:51:41 -0600

    added document query

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ var data = {
   editing: null,
   view: 'view monday'
 };
+
+var $addEntryButton = document.querySelector('.button-add-entry');
+
+var $modalView = document.querySelector('.modal');
+
+var $submitButton = document.querySelector('.button-submit');
+
+$addEntryButton.addEventListener('click', function (event) {
+  $modalView.className = 'modal view';
+
+});
